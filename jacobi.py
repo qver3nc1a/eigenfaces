feat: fixed jacobi rotation direction

jacobi.py
@@ -6,16 +6,20 @@ import numpy as np
 
 def rotation_angle(A, p, q):
     # calculate the rotation angle (tan(2O) = 2a_pq/a_qq-a_pp)
-    return 0.5 * atan2(2 * A[p - 1][q - 1], (A[q - 1][q - 1] - A[p - 1][p - 1]))
+    p -= 1
+    q -= 1
+    return 0.5 * atan2(2 * A[p][q], (A[q][q] - A[p][p]))
 
 
 def rotate_matrix(A, p, q, O):
     # build the rotation matrix R (and return new A with A_pq = 0)
+    p -= 1
+    q -= 1
     R = np.identity(len(A[0]))
-    R[p - 1][p - 1] = cos(O)
-    R[q - 1][q - 1] = cos(O)
-    R[p - 1][q - 1] = -sin(O)
-    R[q - 1][p - 1] = sin(O)
+    R[p][p] = cos(O)
+    R[q][q] = cos(O)
+    R[p][q] = sin(O)
+    R[q][p] = -sin(O)
 
     return np.transpose(R) @ A @ R
 