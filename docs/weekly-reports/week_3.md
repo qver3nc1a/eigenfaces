f0be841d2e30b599f647a85cecc018c1bb9d1340
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-09-16 22:55:55 +0300

    test: add first test to jacobi, update week3 report

---
+++ docs/weekly-reports/week_3.md
@@ -0,0 +1,8 @@
+# Time tracking
+
+|date|time used|description|
+|---|----|------------------|
+|16.09|1h|finish jacobi, read pytest docs and write first tests|
+|total|1h| |
+
+# Report
--- tests/test_jacobi.py
+++ tests/test_jacobi.py
@@ -1,1 +1,8 @@
-# tests for the jacobi algorithm (src/jacobi.py) will be implemented here
+import numpy as np
+from jacobi import rotation_angle, rotate_matrix, off_diagonal, jacobi_cycle, jacobi
+
+
+def test_off_diagonal():
+    A = np.array([[1, 2], [3, 4]])
+    # off-diagonal elements are 2 and 3 -> squares 4+9=13
+    assert off_diagonal(A) == 13
